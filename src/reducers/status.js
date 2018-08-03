7c94d6e3400bc4b2af2ce0523161feef72e18416
kowin <kowinauyeung@gmail.com>
2018-08-03 18:36:03 +0800

    Install redux Update App test

---
+++ src/reducers/status.js
@@ -0,0 +1,12 @@
+const iniState = {
+  isFetchingData: false,
+};
+
+const status = (state = iniState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default status;
