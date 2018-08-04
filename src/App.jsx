98d87f5981e532a007f90cc60f1022dcfe598e26
kowin <kowinauyeung@gmail.com>
2018-08-04 18:24:50 +0800

    Add unit test of search bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <SearchBar onChange={(e) => {console.log(e);}} />
+      <SearchBar onChange={(e) => { console.log(e); }} />
     </div>
   );
 }
