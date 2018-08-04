2fb55cb882d57356bcc894641b20675afec15688
kowin <kowinauyeung@gmail.com>
2018-08-05 04:22:21 +0800

    Add TopList

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
 import RecommendedList from './components/RecommendedList';
+import TopList from './components/TopList';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <SearchBar onChange={(e) => { console.log(e); }} />
       <RecommendedList />
+      <TopList />
     </div>
   );
 }
